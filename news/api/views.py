2b1f345cc4bf1a9f28a33066c2a205b1a2ca84b0
YashRaj1506 <yashraj504300@gmail.com>
2024-11-19 02:07:36 +0530

    written drf views

--- news/api/views.py
+++ news/api/views.py
@@ -11,3 +11,21 @@ from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
 def homepage(request):
     return render(request, 'index.html')
 
+@csrf_exempt
+def data_list(request):
+    """
+    Lists all data on the Web UI
+    """
+
+    if request.method == 'GET':
+        snippets = News_data.objects.all()
+        serializer = Data_Serializer(News_data, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = Data_Serializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
