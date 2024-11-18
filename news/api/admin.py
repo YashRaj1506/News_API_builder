36299a39d83d67db703619ad776f095519b9d629
YashRaj1506 <yashraj504300@gmail.com>
2024-11-19 02:35:01 +0530

    apis are now running with response

--- news/api/admin.py
+++ news/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import News_data
 
-# Register your models here.
+
+admin.site.register(News_data)
--- news/api/views.py
+++ news/api/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render
-from news.api.models import News_data
+from .models import News_data
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from news.api.models import News_data
-from news.api.serializers import Data_Serializer
+# from news.api.models import News_data
+from .serializers import Data_Serializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 
 from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
 
 def homepage(request):
     return render(request, 'index.html')
 
-@csrf_exempt
+@api_view(['GET','POST'])
+@permission_classes([AllowAny])
 def data_list(request):
     """
     Lists all data on the Web UI
     """
 
     if request.method == 'GET':
-        snippets = News_data.objects.all()
-        serializer = Data_Serializer(News_data, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        data = News_data.objects.all()
+        serializer = Data_Serializer(data, many=True)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
@@ -28,4 +33,4 @@ def data_list(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=400)
