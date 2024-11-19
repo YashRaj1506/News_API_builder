7f96ea5f8def18aa4c42a14c04298fedbaad472b
YashRaj1506 <yashraj504300@gmail.com>
2024-11-19 14:27:05 +0530

    removed modelserializers, due to latency issues

--- news/api/models.py
+++ news/api/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class News_data(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=500)
     type = models.CharField(max_length=50)
-    content = models.CharField(max_length=500)
+    content = models.CharField(max_length=5000)
--- news/api/serializers.py
+++ news/api/serializers.py
@@ -1,32 +1,30 @@
 from rest_framework import serializers
 from .models import News_data
 
+# class Data_Serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = News_data
+#         fields = ['title', 'type', 'content']
 
+class Data_Serializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=100)
+    type = serializers.CharField(max_length=50)
+    content = serializers.CharField(max_length=500)
 
-class Data_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = News_data
-        fields = ['title', 'type', 'content']
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return News_data.objects.create(**validated_data)
 
-# class Data_Serializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(max_length=100)
-#     type = serializers.CharField(max_length=50)
-#     content = serializers.CharField(max_length=500)
-#
-#     def create(self, validated_data):
-#         """
-#         Create and return a new `Snippet` instance, given the validated data.
-#         """
-#         return News_data.objects.create(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         """
-#         Update and return an existing `Snippet` instance, given the validated data.
-#         """
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.type = validated_data.get('type', instance.type)
-#         instance.content = validated_data.get('content', instance.content)
-#         instance.save()
-#         return instance
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.type = validated_data.get('type', instance.type)
+        instance.content = validated_data.get('content', instance.content)
+        instance.save()
+        return instance
 
