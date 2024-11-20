b64d6b965a6e7d4b5ebcead73efa15cba8bde8f0
YashRaj1506 <yashraj504300@gmail.com>
2024-11-20 20:05:53 +0530

    field choices was removed from model

--- news/api/models.py
+++ news/api/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class News_data(models.Model):
-    CATEGORY_CHOICES = [
-        ('sports', 'Sports'),
-        ('politics', 'Politics'),
-        ('technology', 'Technology'),
-        ('economy', 'Economy'),
-    ]
+    # CATEGORY_CHOICES = [
+    #     ('sports', 'Sports'),
+    #     ('politics', 'Politics'),
+    #     ('technology', 'Technology'),
+    #     ('economy', 'Economy'),
+    # ]
     title = models.CharField(max_length=500)
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='news')
+    category = models.CharField(max_length=50)
     content = models.CharField(max_length=5000)
 
     def __str__(self):
